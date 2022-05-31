9d2f11de9ec297f214c9911bccd9522614dcab35
karma <karmatashi954@gmail.com>
2022-05-31 09:46:28 +0545

    add of files and basic setup for the application

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by karma on 5/31/22.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
